b0db345a319833f00a55584467ac253b9bc40afe
amccodes <murianto013@gmail.com>
2025-02-04 12:34:21 +0700

    update: fix config environment

--- config/config.go
+++ config/config.go
@@ -16,15 +16,17 @@ func SetupConfig() error {
 	var configuration *Configuration
 	viper.AutomaticEnv()
 
-	viper.SetConfigFile(".env")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+
 	if err := viper.ReadInConfig(); err != nil {
-		logger.Errorf("Error to reading config file, %s", err)
-		return err
+		logger.Warnf("Tidak ada file .env, menggunakan environment variables")
 	}
 
 	err := viper.Unmarshal(&configuration)
 	if err != nil {
-		logger.Errorf("error to decode, %v", err)
+		logger.Errorf("Error saat decode config, %v", err)
 		return err
 	}
 
